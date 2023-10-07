b91eede7b3557cb0aa9409cda92b7506e5a28914
Ulti SPA <ventas@ulti.cl>
2023-10-07 18:14:31 -0300

    titles productos

--- src/app/modules/shop/pages/page-product/page-product.component.ts
+++ src/app/modules/shop/pages/page-product/page-product.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../../../../shared/interfaces/product';
 import { ActivatedRoute } from '@angular/router';
 import { ShopService } from '../../../../shared/api/shop.service';
 import { Observable } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-page-product',
@@ -17,6 +18,7 @@ export class PageProductComponent implements OnInit {
     sidebarPosition: 'start'|'end' = 'start'; // For LTR scripts "start" is "left" and "end" is "right"
 
     constructor(
+        private titleService: Title,
         private shop: ShopService,
         private route: ActivatedRoute,
     ) { }
@@ -28,10 +30,13 @@ export class PageProductComponent implements OnInit {
             this.layout = data['layout'] || this.layout;
             this.sidebarPosition = data['sidebarPosition'] || this.sidebarPosition;
 
+            this.product = this.route.snapshot.data["datos"];
+
+            this.titleService.setTitle(this.product.name + ' | ' + this.product.oem || 'Producto no encontrado');
+
 
             console.log(this.route.snapshot.data)
 
-            this.product = this.route.snapshot.data["datos"];
 
             this.shop.getFeaturedProducts().subscribe((e: any) => {
                 this.products = e;
